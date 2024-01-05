f354f6637a8f27fdb08894759722019a328673db
Brian Haas <brianjohnhaas@gmail.com>
2024-01-05 10:08:21 -0500

    reorg for HGSOC w/ revised ctat-LRF and LRF_FI for v0.12.0

---
+++ 4.SingleCellFusions/4b.sc_HGSOC/consolidate_sc_HGSOC_fusion_predictions/data/Patient2_Tum/update_ctatLR_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+PREFIX="Patient2_Tum"
+CTAT_LR_DIR="/seq/RNASEQ/FUSION_LONGREADS/CTAT_LR_Fusion-paper/tumor_single_cells/HGSOC/HGSOC.Patient2"
+
+# files to be updated:
+
+# -rw-r--r--  1 bhaas  staff   70943 Jan  2 11:22 Patient1_Om.ctat-LR-fusion.fusion_predictions.abridged.tsv.gz
+# -rw-r--r--  1 bhaas  staff  161418 Jan  2 11:22 Patient1_Om.ctat-LR-fusion.fusion_predictions.tsv.gz
+
+# -rw-r--r--  1 bhaas  staff   73807 Jan  2 11:22 Patient1_Om.ctatLRF_FI.fusion_predictions.abridged.tsv.gz
+# -rw-r--r--  1 bhaas  staff  522853 Jan  2 11:22 Patient1_Om.ctatLRF_FI.fusion_predictions.tsv.gz
+
+
+
+
+# CTAT-LR (no FI)
+
+scp login:/${CTAT_LR_DIR}/HGSOC.Patient2.Tum/HGSOC.Patient2.Tum.v0.12.0.LRF/ctat-LR-fusion.fusion_predictions.abridged.tsv.gz ${PREFIX}.ctat-LR-fusion.fusion_predictions.abridged.tsv.gz
+
+scp login:/${CTAT_LR_DIR}/HGSOC.Patient2.Tum/HGSOC.Patient2.Tum.v0.12.0.LRF/ctat-LR-fusion.fusion_predictions.tsv.gz ${PREFIX}.ctat-LR-fusion.fusion_predictions.tsv.gz
+
+# CTAT-LRF_FI
+
+scp login:/${CTAT_LR_DIR}/HGSOC.Patient2.Tum/HGSOC.Patient2.Tum.v0.12.0.LRF_FI/ctat-LR-fusion.fusion_predictions.abridged.tsv.gz ${PREFIX}.ctatLRF_FI.fusion_predictions.abridged.tsv.gz
+
+scp login:/${CTAT_LR_DIR}/HGSOC.Patient2.Tum/HGSOC.Patient2.Tum.v0.12.0.LRF_FI/ctat-LR-fusion.fusion_predictions.tsv.gz ${PREFIX}.ctatLRF_FI.fusion_predictions.tsv.gz
+
