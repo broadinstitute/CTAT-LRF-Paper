312747fdc33bdbf939ca82835b9097db05f03941
Brian Haas <brianjohnhaas@gmail.com>
2024-01-23 11:31:09 -0500

    relocated Alvins scisorWiz fig stuff to git@github.com:MethodsDev/mdl_projects_2023.git/08162023_melanoma_Tcell_mas-seq_fusion_sc/Alvin_ScissorWiz_fig

--- 4.SingleCellFusions/4.sc_fusion_supp/ScisorWiz_short.R
+++
@@ -1,6 +0,0 @@
-library(ScisorWiz) 
-gencodeAnnoFile<- "igv_M132TS.NUTM2A-AS1--RP11-203L2.4/igv.annot.gtf.gz"
-cTypeFile<- "celltype.tab" 
-gffFile<- "readinfo.gff.gz"
-genesFile<- "readstogenes_short.gz"
-ScisorWiz_2File(gencodeAnno=gencodeAnnoFile, gffInput=gffFile, genesInput=genesFile, cellTypeFile=cTypeFile, gene="NUTM2A-AS1--RP11-203L2.4",ci=.05,outputDir="outputDir/")
