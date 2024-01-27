aabecbe1e202a95f5b433479891a8e1edabf9234
Brian Haas <brianjohnhaas@gmail.com>
2024-01-27 11:23:53 -0500

    adding section on ctat-mm2 timing improvements

--- 3.DepMap9Lines/3b.DepMap9Lines_Benchmarking/data/update_files.sh
+++ 3.DepMap9Lines/3b.DepMap9Lines_Benchmarking/data/update_files.sh
@@ -8,19 +8,43 @@ gzip -f preds.collected.gencode_mapped.wAnnot
 
 scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/preds.collected.gencode_mapped.wAnnot.filt.pass .
 
-scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_2_agree/min_2.okPara_ignoreUnsure.results.scored .
-
-scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_2_agree/min_2.okPara_ignoreUnsure.results.scored.ROC .
-
-scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_2_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_2.truth_set .
-
-scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_2_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_2.unique_set .
-
-scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_2_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_2.unsure_set .
 
 
-scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_2_agree/min_2.ignoreUnsure.results.scored .
+## min 2 agree analysis
 
-scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_2_agree/min_2.ignoreUnsure.results.scored.ROC .
+X=2
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/min_${X}.okPara_ignoreUnsure.results.scored .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/min_${X}.okPara_ignoreUnsure.results.scored.ROC .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_${X}.truth_set .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_${X}.unique_set .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_${X}.unsure_set .
 
 
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/min_${X}.ignoreUnsure.results.scored .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/min_${X}.ignoreUnsure.results.scored.ROC .
+
+
+## min 3 agree analysis
+
+X=3
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/min_${X}.okPara_ignoreUnsure.results.scored .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/min_${X}.okPara_ignoreUnsure.results.scored.ROC .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_${X}.truth_set .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_${X}.unique_set .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_${X}.unsure_set .
+
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/min_${X}.ignoreUnsure.results.scored .
+
+scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/cancer_cell_lines/__min_${X}_agree/min_${X}.ignoreUnsure.results.scored.ROC .
