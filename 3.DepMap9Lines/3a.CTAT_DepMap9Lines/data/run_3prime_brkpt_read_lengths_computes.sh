82f9ea4ba1c652693dc7b4f4f6d69b9f830611c8
Brian Haas <brianjohnhaas@gmail.com>
2024-02-10 13:40:25 -0500

    update, include both breakpoints

---
+++ 3.DepMap9Lines/3a.CTAT_DepMap9Lines/data/run_3prime_brkpt_read_lengths_computes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+for cell_line in DMS53 HCC1187 HCC1395 K562 KIJK MJ RT112 SKBR3 VCAP; do
+    ../scripts/examine_fusion_brkpt_3prime_read_dist.py --fusion_predictions ${cell_line}.ctat-LR-fusion.fusion_predictions.tsv.gz --gff3_read_alignments ${cell_line}.LR-FI.mm2.gff3.gz --output ${cell_line}.3prime_brkpt_read_lengths.tsv
+    echo done $cell_line
+done
+
