f50f0089bd372ec0cdecfbbb70c581b0bdb230e8
Brian Haas <brianjohnhaas@gmail.com>
2024-07-01 13:49:03 -0400

    update to jaffal sim reporting

--- 1.Benchmark_Simulated_Fusions/1a.jaffalpaper_simulated_reads/data/update_files.sh
+++ 1.Benchmark_Simulated_Fusions/1a.jaffalpaper_simulated_reads/data/update_files.sh
@@ -5,7 +5,13 @@ set -ex
 
 # allow_reverse.combined_results.ROC.tsv strict.combined_results.ROC.tsv
 
-scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/simulated_data/sim_jaffal/__analyze_strict/combined_results.ROC.tsv strict.combined_results.ROC.tsv
 
-scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/simulated_data/sim_jaffal/__analyze_allow_reverse/combined_results.ROC.tsv allow_reverse.combined_results.ROC.tsv
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/simulated_data/sim_jaffal/__analyze_strict/combined_results.ROC.tsv strict.combined_results.ROC.tsv
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/simulated_data/sim_jaffal/__analyze_allow_reverse/combined_results.ROC.tsv allow_reverse.combined_results.ROC.tsv
+
+
+#scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/simulated_data/sim_jaffal/__analyze_strict/combined_results.ROC.tsv strict.combined_results.ROC.tsv
+
+#scp login://home/unix/bhaas/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/simulated_data/sim_jaffal/__analyze_allow_reverse/combined_results.ROC.tsv allow_reverse.combined_results.ROC.tsv
 
