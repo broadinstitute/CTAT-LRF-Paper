5f983c211030856198c2069b965dbd56235dfb76
Brian Haas <brianjohnhaas@gmail.com>
2024-08-16 11:46:05 -0400

    adding __bmark_min-2-reads/data

---
+++ 3.DepMap9Lines/3b.DepMap9Lines_Benchmarking/__bmark_min-2-reads/data/update_files.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ex
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/preds.collected.gencode_mapped.wAnnot .
+
+gzip -f preds.collected.gencode_mapped.wAnnot
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/preds.collected.gencode_mapped.wAnnot.filt.pass .
+
+
+## min X agree analysis
+
+
+for X in 2 3 4; do
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_${X}.truth_set .
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_${X}.unique_set .
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.min_${X}.unsure_set .
+
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/min_${X}.okPara_ignoreUnsure.results.scored .
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/min_${X}.okPara_ignoreUnsure.results.scored.ROC .
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/min_${X}.okPara_ignoreUnsure.results.scored.PR .
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/min_${X}.okPara_ignoreUnsure.results.scored.PR.AUC .
+
+
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/min_${X}.ignoreUnsure.results.scored .
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/min_${X}.ignoreUnsure.results.scored.ROC .
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/min_${X}.ignoreUnsure.results.scored.PR .
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/__min_${X}_agree/min_${X}.ignoreUnsure.results.scored.PR.AUC .
+
+done
+
+
