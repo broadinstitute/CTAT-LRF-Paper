dd0914516c50cfab1d5c9e12d9a026b1f73549d3
Brian Haas <brianjohnhaas@gmail.com>
2024-08-16 11:28:48 -0400

    add illum

---
+++ 3.DepMap9Lines/3b.DepMap9Lines_Benchmarking/3b.2.Illumina_TP_unique_FP_bmarking/__illum_TP_uniq_FP.starF/data/update_files.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -ex
+
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments .
+
+cp  ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree .
+
+gzip -f preds.collected.gencode_mapped*
+
+
+
+DIRNAME="__illum_TP_uniq_FP.starF"
+
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.illum_agree.truth_set .
+
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/preds.collected.gencode_mapped.wAnnot.filt.pass.proxy_assignments.byProgAgree.nonunique.unsure_set .
+
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/eval_illum_supported.ignoreUnsure.results.scored .
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/eval_illum_supported.ignoreUnsure.results.scored.ROC .
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/eval_illum_supported.ignoreUnsure.results.scored.PR .
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/eval_illum_supported.ignoreUnsure.results.scored.PR.AUC .
+
+
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/eval_illum_supported.okPara_ignoreUnsure.results.scored .
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/eval_illum_supported.okPara_ignoreUnsure.results.scored.ROC .
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/eval_illum_supported.okPara_ignoreUnsure.results.scored.PR .
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/DepMap_Cell_Lines/${DIRNAME}/eval_illum_supported.okPara_ignoreUnsure.results.scored.PR.AUC .
+
+
+
+
+
+
+
+
+
+
+
+
