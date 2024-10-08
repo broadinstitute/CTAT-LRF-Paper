dbb5d2fae11f0d9ed3dacae7b5961e1cde824ca4
Brian Haas <brianjohnhaas@gmail.com>
2024-10-08 13:05:47 -0400

    update for v1.0.0

---
+++ 1.Benchmark_Simulated_Fusions/1c.sim_paralog_fusions/data/update_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/simulated_data/paralog_fusion_sim/__analyze_allow_reverse/sim_paralogs/fusion_preds.txt.scored.ROC analyze_allow_reverse.fusion_preds.txt.scored.ROC
+
+
+cp ~/GITHUB/CTAT_FUSIONS/LR-FusionBenchmarking/simulated_data/paralog_fusion_sim/__analyze_allow_rev_and_paralogs/sim_paralogs/fusion_preds.txt.scored.ROC analyze_allow_rev_and_paralogs.fusion_preds.txt.scored.ROC
+
